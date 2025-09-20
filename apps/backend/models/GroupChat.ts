1bfbd4f21d5fcd71e2611c9b090e0f5f4da54464
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2025-09-20 13:36:41 -0400

    Initialized GrouChat model

---
+++ apps/backend/models/GroupChat.ts
@@ -0,0 +1,29 @@
+import { list } from '@keystone-6/core';
+import type { Lists } from '.keystone/types';
+import { relationship, text, timestamp, checkbox } from '@keystone-6/core/fields';
+
+export const GroupChat = list({
+  access: {
+    operation: {
+      query: () => true,
+      create: () => true,
+      update: ({ session }) => !!session,
+      delete: ({ session }) => !!session,
+    },
+    filter: {
+      query: ({ session }) => ({
+        id: { equals: session.data.id },
+      }),
+    },
+    item: {
+      update: ({ session, item }) => item.id === session.data.id,
+      delete: ({ session, item }) => item.id === session.data.id,
+    },
+  },
+  fields: {
+    groupName: text({ validation: { isRequired: true } }),
+    createdAt: timestamp({
+      defaultValue: { kind: 'now' },
+    }),
+  },
+});
