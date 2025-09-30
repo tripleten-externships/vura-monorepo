0a4db871172b978f25c41c4d7b15276697a0d6ad
bencorpus <bencorpus7799@gmail.com>
2025-09-29 22:55:38 -0400

    Implement Resource and Checklist models

---
+++ apps/backend/models/resource.ts
@@ -0,0 +1,19 @@
+import { list } from '@keystone-6/core';
+import { text, relationship } from '@keystone-6/core/fields';
+
+export const Resource = list({
+  idField: { kind: 'autoincrement' },
+  fields: {
+    link: text({ validation: { isRequired: true } }),
+    content: text({ validation: { isRequired: true } }),
+    checklist: relationship({ ref: 'Checklist.resources' }),
+  },
+  access: {
+    operation: {
+      query: () => true,
+      create: ({ session }) => !!session,
+      update: ({ session }) => !!session,
+      delete: ({ session }) => !!session,
+    },
+  },
+});
