625c3d9680c15c97f9b8850ac9f91f62fd12e054
Alison de la Paz <arhibschle@gmail.com>
2025-10-02 13:01:47 -0500

    Vura 3 user model schema (#33)
    * completed all fields in user schema
    
    * Parent + Careplan.ts additions
    
    * [VURA-3] fix migrations after merge
    
    * [VURA-3] synchronize migrations
    
    * [VURA-3] cleaning up duped schema and model data
    
    * [VURA-3] fix migration drift
    
    * Vura-3 age and gender requirement adjustment
    
    * [VURA-3] sync migrations, toggle off avatar validation
    
    ---------
    
    Co-authored-by: costjonah <jonah.cost@tripleten.com>

--- apps/backend/models/parent.ts
+++ apps/backend/models/parent.ts
@@ -1,7 +1,7 @@
 import { User } from './user';
 
 import { list } from '@keystone-6/core';
-import { text, integer, relationship, json, timestamp } from '@keystone-6/core/fields';
+import { text, integer, relationship, json, timestamp, select } from '@keystone-6/core/fields';
 
 export const Parent = list({
   //only logged in users can create, update, and delete
@@ -29,20 +29,20 @@ export const Parent = list({
     name: text({
       validation: { isRequired: true },
     }),
-
     age: integer({
-      validation: { isRequired: true, min: 0 },
+      validation: { isRequired: true },
     }),
 
     relationship: text({
       validation: { isRequired: true },
     }),
-    health_conditions: json(),
 
-    created_at: timestamp({
+    healthConditions: json(),
+
+    createdAt: timestamp({
       defaultValue: { kind: 'now' },
     }),
-    updated_at: timestamp({
+    updatedAt: timestamp({
       db: { updatedAt: true },
     }),
     user: relationship({
