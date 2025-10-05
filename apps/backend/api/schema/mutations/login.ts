d07f964dfbb1dd3d77a2c935a2ec9f072128bd50
sun4205 <hanna4205@gmail.com>
2025-10-05 18:40:21 -0400

    [VURA-20]create login mutation

--- apps/backend/api/schema/mutations/login.ts
+++ apps/backend/api/schema/mutations/login.ts
@@ -1,1 +1,84 @@
 // Login -> JWT/session
+import { GraphQLError } from 'graphql';
+import { Context } from '../../../types/context';
+import bcrypt from 'bcryptjs';
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
+
+// Stores the record of login attempts
+const loginAttempts: Record<string, number> = {};
+
+// Max number of login attempts
+const maxNumberLogin = 5;
+
+// Created a function to check for maximum login attempts
+function checkLoginAttempts(email: string) {
+  if (loginAttempts[email] >= maxNumberLogin) {
+    throw new GraphQLError('Too many login attempts. Please try again later');
+  }
+}
+
+export const login = async (root: any, { input }: { input: LoginInput }, context: Context) => {
+  const { email, password } = input;
+
+  // If there is no login record, create one.
+  if (!loginAttempts[email]) loginAttempts[email] = 0;
+
+  // Check missing required fields
+  if (!email || !password) {
+    loginAttempts[email]++;
+    checkLoginAttempts(email);
+    throw new GraphQLError('Email and password are required');
+  }
+
+  // Check if user doesn't exist
+  const user = await context.prisma.user.findUnique({
+    where: { email },
+  });
+  if (!user) {
+    loginAttempts[email]++;
+    checkLoginAttempts(email);
+    throw new GraphQLError('Account not found');
+  }
+
+  // Check for incorrect credentials
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    loginAttempts[email]++;
+    checkLoginAttempts(email);
+    throw new GraphQLError('invalid password');
+  }
+
+  // When login is successful or after reaching max attempts, reset count to 0.
+  loginAttempts[email] = 0;
+
+  try {
+    const sessionData = await context.sessionStrategy?.start({
+      data: { email: user.email },
+      context,
+    });
+
+    return {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatarUrl: user.avatarUrl,
+        age: user.age,
+        gender: user.gender,
+        privacyToggle: user.privacyToggle,
+        isAdmin: user.isAdmin,
+        createdAt: user.createdAt,
+        lastLoginDate: user.lastLoginDate,
+        lastUpdateDate: user.lastUpdateDate,
+      },
+      token: sessionData,
+    };
+  } catch (error) {
+    console.error('Login error:', error);
+    throw new GraphQLError('Login Failed');
+  }
+};
