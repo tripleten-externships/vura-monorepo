31c4fcc50510ad321b3d45cdd76cf543071bab40
holden <james.holden.moore@gmail.com>
2025-10-08 15:17:33 -0400

    [VURA-36] created careplan model

--- apps/backend/models/care-plan.ts
+++ apps/backend/models/care-plan.ts
@@ -1,18 +1,43 @@
 import { list } from '@keystone-6/core';
 // import type { Lists } from '.keystone/types';
-import { text, relationship } from '@keystone-6/core/fields';
+import { text, timestamp, relationship } from '@keystone-6/core/fields';
+import { allowAll } from '@keystone-6/core/access';
 
 export const CarePlan = list({
   access: {
     operation: {
-      query: () => true,
-      create: () => true,
+      // anyone can query
+      query: allowAll,
+      // must be logged in
+      create: ({ session }) => !!session?.data,
       update: ({ session }) => !!session,
       delete: ({ session }) => !!session,
     },
   },
   fields: {
-    name: text({ validation: { isRequired: true } }),
-    user: relationship({ ref: 'User.carePlan' }),
+    title: text({
+      isIndexed: 'unique',
+      validation: { isRequired: true },
+    }),
+    user: relationship({
+      // users careplan list
+      ref: 'User.carePlans',
+      many: false,
+    }),
+    createdAt: timestamp({
+      defaultValue: { kind: 'now' },
+      ui: { createView: { fieldMode: 'hidden' } },
+    }),
+    updatedAt: timestamp({
+      // auto-update on save
+      db: { updatedAt: true },
+      ui: { createView: { fieldMode: 'hidden' } },
+    }),
+  },
+  ui: {
+    labelField: 'title',
+    listView: {
+      initialColumns: ['title', 'user', 'createdAt', 'updatedAt'],
+    },
   },
 });
