a26564eaf3ae276442883d1d2e2e9a3585bcbf4d
Monet <codem0n3t@gmail.com>
2025-10-08 03:44:06 -0400

    mutation for sending a chat message

--- apps/backend/api/resolvers/Mutation.ts
+++ apps/backend/api/resolvers/Mutation.ts
@@ -1,6 +1,8 @@
 // stitches mutation resolvers together
 import { createGroupChat } from '../schema/mutations/createGroupChat';
+import { sendChatMessage } from '../schema/mutations/sendChatMessage';
 
 export const Mutation = {
   createGroupChat,
+  sendChatMessage,
 };
--- apps/backend/api/schema/typeDefs.ts
+++ apps/backend/api/schema/typeDefs.ts
@@ -13,8 +13,29 @@ export const typeDefs = gql`
     message: String!
   }
 
+  input CreateGroupChatInput {
+    groupName: String!
+    memberIds: [ID!]!
+  }
+
+  type CreateGroupChatResult {
+    groupChat: GroupChat!
+    message: String!
+  }
+
+  input SendChatMessageInput {
+    groupId: ID!
+    message: String!
+  }
+
+  type SendChatMessageResult {
+    chatMessage: ChatMessage!
+    message: String!
+  }
+
   extend type Mutation {
     createGroupChat(input: CreateGroupChatInput!): CreateGroupChatResult!
+    sendChatMessage(input: SendChatMessageInput!): SendChatMessageResult!
   }
 `;
 export const resolvers = {
