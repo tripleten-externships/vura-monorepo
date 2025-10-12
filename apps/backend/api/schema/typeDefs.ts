53220e73bbe4efd1f5a44940128d7c44cb8e2c76
Alison de la Paz <arhibschle@gmail.com>
2025-10-12 10:09:56 -0400

    defining createForumPost in typeDefs.ts

--- apps/backend/api/schema/typeDefs.ts
+++ apps/backend/api/schema/typeDefs.ts
@@ -1,13 +1,23 @@
 import { gql } from 'graphql-tag';
-// SDL for custom types/inputs/enums
-export const typeDefs = gql`
-  scalar DateTime
 
-  type Mutation {
-    _empty: String
+export const typeDefs = gql`
+  type ForumPost {
+    id: ID!
+    title: String!
+    topic: String
+    content: String!
+    author: String
+    createdAt: String!
+    updatedAt: String!
   }
 
   type Query {
-    _empty: String
+    getForumPost(id: ID!): ForumPost
+  }
+
+  type Mutation {
+    createForumPost(title: String!, topic: String, content: String!, author: String): ForumPost
   }
 `;
+
+// typeDefs describe what types of data the API exposes and what operations are allowed
