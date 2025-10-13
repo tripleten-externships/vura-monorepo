b30b69c02744be2bb3f6cf3f7c43f7b4f7a86924
Alison de la Paz <arhibschle@gmail.com>
2025-10-13 09:51:07 -0400

    [VURA-27} getForumPost edits to typeDefs

--- apps/backend/api/schema/typeDefs.ts
+++ apps/backend/api/schema/typeDefs.ts
@@ -1,13 +1,42 @@
 import { gql } from 'graphql-tag';
 // SDL for custom types/inputs/enums
 export const typeDefs = gql`
-  scalar DateTime
-
-  type Mutation {
-    _empty: String
+  input GetForumPostsInput {
+    first: Int
+    after: String
+    topic: String
+    authorId: ID
+    searchTerm: String
+    dateFrom: DateTime
+    dateTo: DateTime
+    orderBy: ForumPostOrderBy
+  }
+  enum ForumPostOrderBy {
+    CREATED_AT_ASC
+    CREATED_AT_DESC
+    UPDATED_AT_ASC
+    UPDATED_AT_DESC
+    TITLE_ASC
+    TITLE_DESC
   }
 
-  type Query {
-    _empty: String
+  type ForumPostConnection {
+    edges: [ForumPostEdge!]!
+    pageInfo: PageInfo!
+    totalCount: Int!
+  }
+
+  type ForumPostEdge {
+    node: ForumPost!
+    cursor: String!
+  }
+  type PageInfo {
+    hasNextPage: Boolean!
+    hasPreviousPage: Boolean!
+    startCursor: String
+    endCursor: String
+  }
+  extend type Query {
+    getForumPosts(input: GetForumPostsInput): ForumPostConnection!
   }
 `;
