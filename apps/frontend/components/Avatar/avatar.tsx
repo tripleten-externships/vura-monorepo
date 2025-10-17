3ca163c4a4b94c6b4a01e860442210a3522d0fd0
Alison de la Paz <arhibschle@gmail.com>
2025-10-16 20:06:30 -0400

    [VURA-68]avatar-nativewind-refactor

--- apps/frontend/components/Avatar/avatar.tsx
+++ apps/frontend/components/Avatar/avatar.tsx
@@ -9,31 +9,14 @@ interface AvatarProps {
 const Avatar: React.FC<AvatarProps> = ({ initials, size }) => {
   // FC is optional but tells React that this is a functional component and automaticallly includes children as prop
   const sizeClasses = {
-    sm: { width: 32, height: 32, fontSize: 14 }, // in chat box
-    md: { width: 44, height: 44, fontSize: 18 }, // in forum posts
-    lg: { width: 100, height: 100, fontSize: 40 }, // for profile page
+    sm: 'w-[32px] h-[32px] text-[14px]', // messaging avatar
+    md: 'w-[44px] h-[44px] text-[18px]', // avatar in forum
+    lg: 'w-[100px] h-[100px] text-[40px]', // avatar for edit profile
   };
-  const { width, height, fontSize } = sizeClasses[size];
+
   return (
-    <View
-      style={{
-        width,
-        height,
-        borderRadius: width / 2,
-        backgroundColor: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text
-        style={{
-          color: 'white',
-          fontWeight: 'bold',
-          fontSize,
-        }}
-      >
-        {initials}
-      </Text>
+    <View className={`bg-black rounded-full justify-center items-center ${sizeClasses[size]}`}>
+      <Text className="text-white font-bold">{initials}</Text>
     </View>
   );
 };
