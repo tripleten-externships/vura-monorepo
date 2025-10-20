f9fede5bfc7eb028b554cc9aba6d4c41d5a75f0e
Hanna Lee <130246836+sun4205@users.noreply.github.com>
2025-10-20 15:33:09 -0400

    Vura-62/reusable input field (#62)
    * [VURA-62]create reusable input field
    
    * [VURA-62] Add temporary test components
    
    * [VURA-62]Add temporary test components
    
    * [VURA-62] add type def to placeholderTextColor
    
    ---------
    
    Co-authored-by: costjonah <jonah.cost@tripleten.com>

--- apps/frontend/app/welcome.tsx
+++ apps/frontend/app/welcome.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 // import { router } from 'expo-router';
+import { InputField } from '../src/components/InputField/InputField';
+// import { PostInput } from '../src/components/PostInput/PostInput';
+// import { Emoji } from '../src/components/Emoji/Emoji';
+// import { Bell } from '../src/components/Notification_bell/NotificationBell';
 
 export default function WelcomePage() {
   const handleLogin = () => {
@@ -13,6 +17,68 @@ export default function WelcomePage() {
       <Text style={styles.title}>Welcome to Vura by Betterhunt</Text>
       <Text style={styles.subtitle}>Please log in to continue</Text>
 
+      {/* I added a temporary input field to test the component.*/}
+      <InputField
+        placeholder="Email"
+        placeholderTextColor="rgba(54,54,54,0.5)"
+        containerStyle={{
+          borderColor: '#e7e7e7',
+          paddingHorizontal: 20,
+          marginTop: 20,
+          width: 345,
+          backgroundColor: '#f6f4fa',
+        }}
+      />
+      <InputField
+        placeholder="Password"
+        placeholderTextColor="rgba(54,54,54,0.5)"
+        secureTextEntry
+        containerStyle={{
+          borderColor: '#e7e7e7',
+          paddingHorizontal: 20,
+          marginTop: 20,
+          width: 345,
+          backgroundColor: '#f6f4fa',
+        }}
+      />
+      <InputField
+        placeholderTextColor="rgba(54,54,54,0.5)"
+        placeholder="Ask AI helper, any question"
+        svgIcon={{ uri: '../../assets/arrow_right.png' }}
+        containerStyle={{
+          borderColor: '#e7e7e7',
+          height: 62,
+          paddingHorizontal: 16,
+          marginTop: 20,
+          width: 345,
+          backgroundColor: '#ffffff',
+        }}
+        iconStyle={{ width: 32, height: 32, marginTop: -4 }}
+      />
+      <InputField
+        placeholderTextColor="rgba(54,54,54,0.5)"
+        placeholder="Type your answer"
+        svgIcon={{ uri: '../../assets/Send.png' }}
+        containerStyle={{
+          borderColor: '#e7e7e7',
+          height: 62,
+          paddingHorizontal: 16,
+          marginTop: 20,
+          width: 345,
+          backgroundColor: '#ffffff',
+        }}
+        iconStyle={{ width: 24, height: 24 }}
+      />
+      {/* I added a post input field to test the component.
+      <PostInput
+        placeholderTextColor="rgba(54,54,54,0.5)"
+        titlePlaceholder="Post title"
+        bodyPlaceholder="Post text"
+      /> */}
+      {/* test emoji component
+       <Emoji emojiIcon={{ uri: '../../assets/smile.svg' }} /> */}
+      {/* test bell component 
+      <Bell bellIcon={{ uri: '../../assets/notification_bell.png' }} /> */}
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
