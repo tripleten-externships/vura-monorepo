38f9b1b03d5f3bd265718d7aebe61a0131630da9
sun4205 <hanna4205@gmail.com>
2025-12-11 21:09:30 -0500

    [VURA-117] first commit

--- apps/frontend/src/screens/Checklist/Checklist.tsx
+++ apps/frontend/src/screens/Checklist/Checklist.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { PageHeader } from '../../components/PageHeader/PageHeader';
 import { NotificationBell } from '../../components/NotificationBell/NotificationBell';
 import { useNavigate } from 'react-router-dom';
 import { useUnreadNotifications } from '../../hooks/useUnreadNotifications';
+import Checkbox from '../../components/Checkbox/Checkbox';
+import { Emoji } from '../../components/Emoji/Emoji';
 
 export default function ChecklistScreen() {
   const navigate = useNavigate();
   const { hasUnread } = useUnreadNotifications();
+  const [testChecked, setTestChecked] = useState(false);
   return (
     <View style={styles.container}>
       <PageHeader
         title="Your care action plan"
+        titleStyle={styles.titlestyle}
         rightIcon={{
           icon: (
             <NotificationBell hasUnread={hasUnread} onClick={() => navigate('/notifications')} />
           ),
         }}
       />
+      <Text style={styles.sectionTitle}>Daily</Text>
+      <View style={styles.checkboxContainer}>
+        <Checkbox
+          label="Check in with parent's mood"
+          checked={testChecked}
+          onChange={setTestChecked}
+        />
+
+        <Image source={{ uri: '../../../assets/flower.svg' }} />
+      </View>
+      <View style={styles.checkboxContainer}>
+        <Checkbox label="prepare healthy meals" checked={testChecked} onChange={setTestChecked} />
+
+        <Image source={{ uri: '../../../assets/flower.svg' }} />
+      </View>
+      <Text style={styles.sectionTitle}>Weekly</Text>
+      <View style={styles.checkboxContainer}>
+        <Checkbox
+          label="Check in with parent's mood"
+          checked={testChecked}
+          onChange={setTestChecked}
+        />
+
+        <Image source={{ uri: '../../../assets/flower.svg' }} />
+      </View>
+      <View style={styles.checkboxContainer}>
+        <Checkbox label="prepare healthy meals" checked={testChecked} onChange={setTestChecked} />
+
+        <Image source={{ uri: '../../../assets/flower.svg' }} />
+      </View>
     </View>
   );
 }
@@ -28,8 +62,27 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#f5f5f5',
   },
+  titleStyle: {
+    fontFamily: 'Noto Serif',
+    fontWeight: 400,
+    fontSize: 34,
+    textAlign: 'center',
+    lineHeight: 44,
+  },
   topIcon: {
     width: 100,
     height: 100,
+  },
+  checkboxContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 500,
+    color: 'rgba(54, 54, 54, 0.5)',
+    textAlign: 'center',
+    marginBottom: 16,
+    marginTop: 32,
   },
 });
